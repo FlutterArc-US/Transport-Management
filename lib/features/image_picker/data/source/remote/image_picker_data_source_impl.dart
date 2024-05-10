cea605e6d8369b96098e5a30d5f4ab9fdf7d6bd0
aiman-sartaj <aiman.141.id@gmail.com>
2024-05-10 10:41:18 +0500

    image picker fixed

---
+++ lib/features/image_picker/data/source/remote/image_picker_data_source_impl.dart
@@ -0,0 +1,70 @@
+// Dart imports:
+import 'dart:io';
+
+// Package imports:
+import 'package:image_picker/image_picker.dart';
+import 'package:injectable/injectable.dart';
+
+// Project imports:
+import 'package:transport_management/features/image_picker/domain/data/image_picker_data_source.dart';
+import 'package:transport_management/features/image_picker/utils/large_file_checker.dart';
+import 'package:transport_management/util/exceptions/message_exception.dart';
+
+@LazySingleton(as: ImagePickerHelper)
+class ImagePickerImagePickerHelper implements ImagePickerHelper {
+  final ImagePicker imagePicker = ImagePicker();
+
+  @override
+  Future<List<String>> multiGalleryImages() async {
+    final imagesList = await imagePicker.pickMultiImage();
+    if (imagesList.isEmpty) {
+      throw ImageNotSelectedException();
+    }
+    final selectedPaths = <String>[];
+
+    for (final image in imagesList) {
+      final file = File(image.path);
+      final bytes = await file.readAsBytes();
+
+      /// [When image size is less than or equal to 5MB]
+      if (bytes.lengthInBytes.isMoreThan5MBs()) {
+        throw ImageSizeExceedsSizeLimitException();
+      }
+      selectedPaths.add(image.path);
+    }
+    return selectedPaths;
+  }
+
+  @override
+  Future<String> cameraImage() async {
+    final image = await imagePicker.pickImage(source: ImageSource.camera);
+    if (image == null) {
+      throw ImageNotSelectedException();
+    }
+    return image.path;
+  }
+
+  @override
+  Future<String> galleryImage() async {
+    final image = await imagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) {
+      throw ImageNotSelectedException();
+    } else {
+      // Check if the selected file is an image
+      if (image.path.endsWith('.jpg') ||
+          image.path.endsWith('.jpeg') ||
+          image.path.endsWith('.png')) {
+        final file = File(image.path);
+        final bytes = await file.readAsBytes();
+
+        /// [When image size is less than or equal to 5MB]
+        if (bytes.lengthInBytes.isMoreThan5MBs()) {
+          throw ImageSizeExceedsSizeLimitException();
+        }
+        return image.path;
+      } else {
+        throw ImageFileNotSupportedException();
+      }
+    }
+  }
+}
