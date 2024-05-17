d4d0f12a6060bf8f3b393220fb335f4743bdd69a
aiman-sartaj <aiman.141.id@gmail.com>
2024-05-17 10:34:34 +0500

    bottom nav bar added

--- lib/features/home/presentation/views/widgets/bottom_nav_bar.dart
+++ lib/features/home/presentation/views/widgets/bottom_nav_bar.dart
@@ -1,13 +1,11 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:transport_management/common/extensions/app_localization.dart';
-import 'package:transport_management/common/extensions/num.dart';
-import 'package:transport_management/common/widgets/app_text.dart';
 import 'package:transport_management/features/home/domain/enums/bottom_nav_item.dart';
 import 'package:transport_management/features/home/presentation/providers/selected_nav_item_provider/selected_nav_item_provider.dart';
-import 'package:transport_management/features/home/presentation/views/widgets/circular_nav_bar.dart';
 import 'package:transport_management/util/resources/r.dart';
 
 class AppBottomNavBar extends ConsumerStatefulWidget {
@@ -18,14 +16,7 @@ class AppBottomNavBar extends ConsumerStatefulWidget {
 }
 
 class _AppBottomNavBarState extends ConsumerState<AppBottomNavBar> {
-  int _tabIndex = 0;
-  int get tabIndex => _tabIndex;
-
-  set tabIndex(int v) {
-    final item = ref.read(selectedNavItemProvider);
-    _tabIndex = item.index;
-    setState(() {});
-  }
+  final _controller = NotchBottomBarController();
 
   String navItemName(BottomNavItem item) => switch (item) {
         BottomNavItem.routeToday => context.appLocale.routeToday,
@@ -35,64 +26,35 @@ class _AppBottomNavBarState extends ConsumerState<AppBottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    final currentItem = ref.watch(selectedNavItemProvider);
-
-    return CircleNavBar(
-      activeIcons: List.generate(
+    return AnimatedNotchBottomBar(
+      notchBottomBarController: _controller,
+      removeMargins: true,
+      color: R.colors.green_074834,
+      bottomBarItems: List.generate(
         BottomNavItem.values.length,
         (index) {
-          return SvgPicture.asset(
-            BottomNavItem.values[index].activeIconPath,
-          );
-        },
-      ),
-      inactiveIcons: List.generate(
-        BottomNavItem.values.length,
-        (index) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                BottomNavItem.values[index].inactiveIconPath,
-              ),
-              3.hb,
-              AppText(
-                text: navItemName(BottomNavItem.values[index]),
+          return BottomBarItem(
+            activeItem:
+                SvgPicture.asset(BottomNavItem.values[index].activeIconPath),
+            inActiveItem: SvgPicture.asset(
+              BottomNavItem.values[index].inactiveIconPath,
+            ),
+            itemLabelWidget: Text(
+              navItemName(BottomNavItem.values[index]),
+              style: TextStyle(
                 color: R.colors.white_FFFFFF,
-                textAlign: TextAlign.center,
-              )
-            ],
+                fontSize: 10.sp,
+              ),
+            ),
           );
         },
       ),
-      color: R.colors.white_FFFFFF,
-      circleColor: R.colors.white_FFFFFF,
-      height: 67.h,
-      circleWidth: 60.r,
-      activeIndex: currentItem.index,
-      onTap: (index) {
-        tabIndex = index;
-        ref
-            .read(selectedNavItemProvider.notifier)
-            .update((state) => BottomNavItem.values[index]);
+      onTap: (int value) {
+        ref.read(selectedNavItemProvider.notifier).state =
+            BottomNavItem.values[value];
       },
-      padding: EdgeInsets.only(
-        left: 13.w,
-        right: 13.w,
-        bottom: 20.h,
-      ),
-      cornerRadius: BorderRadius.only(
-        topLeft: Radius.circular(12.r),
-        topRight: Radius.circular(12.r),
-      ),
-      shadowColor: Colors.transparent,
-      elevation: 10,
-      gradient: LinearGradient(
-        colors: [
-          R.colors.blue_305477,
-          R.colors.blue_001020,
-        ],
-      ),
+      kIconSize: 26.r,
+      kBottomRadius: 0,
     );
   }
 }
